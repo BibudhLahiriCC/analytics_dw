6d05eb788306f40c103bde57bc7fbbeadef427c0
Bibudh Lahiri <bibudh@casecommons.org>
2012-03-22 12:15:01 -0400

    Cleaner code in model

--- app/controllers/days_since_last_visits_controller.rb
+++ app/controllers/days_since_last_visits_controller.rb
@@ -2,9 +2,8 @@ class DaysSinceLastVisitsController < ApplicationController
   # GET /days_since_last_visits
   # GET /days_since_last_visits.json
   def index
-    puts "gender = " + params[:gender] unless  params[:gender].nil?
     @days_since_last_visits = DaysSinceLastVisit.histogram params[:gender]
-    #puts @days_since_last_visits.class
+
     respond_to do |format|
       format.html # index.html.erb
       format.json { render :json => @days_since_last_visits }
